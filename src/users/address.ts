b64502efa601c37075257c0e9f9372db7d545747
akigrafsoft <akigrafsoft@gmail.com>
2017-05-12 21:18:46 +0200

    address update

--- src/users/address.ts
+++ src/users/address.ts
@@ -1,6 +1,6 @@
 export class Address {
-    addressLine1: string;
-    addressLine2: string;
+    line1: string;
+    line2: string;
     postalCode: string;
     town: string;
     province: string;
@@ -14,6 +14,17 @@ export class Address {
                     o_object[k] = document[k];
             }
         }
+        return o_object;
+    }
+
+    public static of(a: Address): Address {
+        let o_object = new Address();
+        o_object.line1 = a.line1;
+        o_object.line2 = a.line2;
+        o_object.postalCode = a.postalCode;
+        o_object.town = a.town;
+        o_object.province = a.province;
+        o_object.state = a.state;
         return o_object;
     }
 }
