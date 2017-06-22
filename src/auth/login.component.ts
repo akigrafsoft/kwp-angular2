4f312073900b625135f1403d7ef9df836825d1cd
akigrafsoft <kevin_moyse@yahoo.fr>
2017-06-22 11:29:42 +0200

    login.component update

--- src/auth/login.component.ts
+++ src/auth/login.component.ts
@@ -10,33 +10,30 @@ import { Error } from '../services/error';
 @Component({
     selector: 'auth-login',
     //    templateUrl: 'app/kwp/auth/login.component.html'
-    template: `<div>
- <form name="loginForm" class="form-inline enabled" accept-charset="UTF-8" (ngSubmit)="login()" #f="ngForm">
-  <div class="form-group">
-   <div class="input-group">
-    <span class="input-group-addon" id="username-addon"><span class="glyphicon glyphicon-user"></span></span> <input type="text"
-     class="form-control" id="login-username" name="username" placeholder="{{_l=='fr' ? 'nom utilisateur' : 'username'}}"
-     [(ngModel)]="username" value="" aria-describedby="username-addon" required>
-   </div>
+    template: `<form name="loginForm" [class.form-inline]="inline" accept-charset="UTF-8" (ngSubmit)="login()" #f="ngForm">
+ <div class="form-group">
+  <div class="input-group">
+   <span class="input-group-addon" id="username-addon"><span class="glyphicon glyphicon-user"></span></span> <input type="text"
+    class="form-control" id="login-username" name="username" placeholder="{{_l=='fr' ? 'nom utilisateur' : 'username'}}"
+    [(ngModel)]="username" value="" aria-describedby="username-addon" required>
   </div>
-  <div class="form-group">
-   <div class="input-group">
-    <span class="input-group-addon" id="password-addon"> <span class="glyphicon glyphicon-lock"></span></span> <input
-     type="password" class="form-control" id="login-password" name="password"
-     placeholder="{{_l=='fr' ? 'mot de passe' : 'password'}}" [(ngModel)]="password" aria-describedby="password-addon" required>
-   </div>
+ </div>
+ <div class="form-group">
+  <div class="input-group">
+   <span class="input-group-addon" id="password-addon"> <span class="glyphicon glyphicon-lock"></span></span> <input
+    type="password" class="form-control" id="login-password" name="password"
+    placeholder="{{_l=='fr' ? 'mot de passe' : 'password'}}" [(ngModel)]="password" aria-describedby="password-addon" required>
   </div>
-  <div class="form-group">
-   &nbsp;
-   <button id='login-btn' type="submit" class="btn btn-success" [disabled]="!f.form.valid">{{_l=='fr' ? 'Connexion' :
-    'Login'}}</button>
-  </div>
- </form>
+ </div>
+ <div class="form-group">
+  &nbsp;
+  <button id='login-btn' type="submit" class="btn btn-success" [disabled]="!f.form.valid">{{_l=='fr' ? 'Connexion' :
+   'Login'}}</button>
+ </div>
  <div *ngIf="error">
   <span class="alert alert-warning" title="{{error.code}}">{{error.reason}}</span>
  </div>
-</div>
-<div>`
+</form>`
 })
 export class AuthLoginComponent {
 
@@ -45,6 +42,7 @@ export class AuthLoginComponent {
         this._l = lang;
     }
     @Input() username: string = null;
+    @Input() inline: boolean = false;
     @Output() onLogin = new EventEmitter<boolean>();
     @Output() onError = new EventEmitter<boolean>();
 
