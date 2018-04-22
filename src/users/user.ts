91e549881e446e8a7be5af6dd453098728e4b1e4
akigrafsoft <akigrafsoft@gmail.com>
2018-04-22 13:05:40 +0200

    user: added bdate

--- src/users/user.ts
+++ src/users/user.ts
@@ -9,6 +9,7 @@ export class User {
   public username: string;
   public firstName: string;
   public lastName: string;
+  public bdate: any;
   public email: string;
   public phone: string;
   public address: Address = null;
@@ -60,31 +61,6 @@ export class User {
         }
       }
     }
-
-    //        if (typeof document._id !== 'undefined')
-    //            o_object.id = document._id.$oid;
-    //        o_object.username = document.username;
-    //        o_object.firstName = document.firstName;
-    //        o_object.lastName = document.lastName;
-    //        o_object.email = document.email;
-    //        if (typeof document.phone !== 'undefined')
-    //            o_object.phone = document.phone;
-
-    //    if ((typeof document.address !== 'undefined') && (document.address !== null))
-    //      o_object.address = Address.build(document.address);
-    //    else
-    //      o_object.address = null;
-    //
-    //    if (typeof document.roles !== 'undefined' && document.roles != null) {
-    //      for (var name of document.roles) {
-    //        o_object.roles.push(name);
-    //      }
-    //    }
-
-    //    if (typeof document.activationTimeMillis !== 'undefined')
-    //      o_object.activationTimeMillis = document.activationTimeMillis;
-    //    if (typeof document.lSLTMs !== 'undefined')
-    //      o_object.lSLTMs = document.lSLTMs;
 
     return o_object;
   }
